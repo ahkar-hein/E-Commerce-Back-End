9734c2b555a9fdce47fdd2bab1f2fb954aeec3eb
Ahkar Hein <ahkarhein04@gmail.com>
2023-08-08 11:48:43 -0700

    put route for update the category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,8 +34,21 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: { id: req.params.id },
+    });
+
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'Category not found' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Category updated successfully' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
