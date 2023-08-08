3f069a85c1f459a3c3e602cd0de39c6054822487
Ahkar Hein <ahkarhein04@gmail.com>
2023-08-08 12:32:45 -0700

    get route for find single product by id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -16,9 +16,15 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const ProductData = await Product.findByPk(req.params.id, {
+      include: [{model: Category}, {model: Tag}],
+    })
+    res.status(200).json(ProductData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
