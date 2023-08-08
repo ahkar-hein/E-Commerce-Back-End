c116fb59e82091f03174f316ef081932b6ca9753
Ahkar Hein <ahkarhein04@gmail.com>
2023-08-08 12:34:08 -0700

    delete route for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -104,8 +104,20 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const ProductData = await Product.destroy({
+      where: { id: req.params.id},
+    });
+
+    if (!ProductData) {
+      res.status(404).json({ message: 'No Product found with that id!' });
+      return;
+    }
+    res.status(200).json({ message: 'Product deleted successfully' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
